19a58bd378af69433007b1f7ed5bf1c7c744909a
zytsev <zaes2000@mail.ru>
2023-09-15 16:40:46 +0100

    feat: added animatin text and control LANG=en_EN date (Sep 15, 2023 16-39)

---
+++ index.js
@@ -0,0 +1,70 @@
+// controlseffect start
+const WrapperPoints = document.querySelector('.wrapper-points')
+const PointArea = document.querySelectorAll('.point-area')
+const Points = document.querySelectorAll('.point')
+const Texts = document.querySelectorAll('.text')
+const Picture = document.querySelectorAll('.carousel')
+
+//наводим курсор на выделенную кнопку
+WrapperPoints.addEventListener('mouseover', (event) => {
+    for (let i = 0; i < PointArea.length; i++) {
+        if (PointArea[i] === event.target && Points[i].classList.contains('ring')) {
+            Points[i].classList.add('border-white')
+        }
+    }
+})
+
+//убираем курсор с выделенной кнопки
+WrapperPoints.addEventListener('mouseout', (event) => {
+    for (let i = 0; i < PointArea.length; i++) {
+        if (PointArea[i] === event.target && Points[i].classList.contains('ring') && Points[i] !== event.relatedTarget) {
+            Points[i].classList.remove('border-white')
+        }
+    }
+})
+
+//наводим курсор на не выделенную кнопку
+WrapperPoints.addEventListener('mouseover', (event) => {
+    for (let i = 0; i < PointArea.length; i++) {
+        if (PointArea[i] === event.target && !Points[i].classList.contains('ring')) {
+            Points[i].classList.add('back-white')
+        }
+    }
+})
+
+//убираем курсор с не выделенной кнопки
+WrapperPoints.addEventListener('mouseout', (event) => {
+    for (let i = 0; i < PointArea.length; i++) {
+        if (PointArea[i] === event.target && !Points[i].classList.contains('ring') && Points[i].classList.contains('back-white') && Points[i] !== event.relatedTarget) {
+            Points[i].classList.remove('back-white')
+        }
+    }
+})
+
+// клик на кнопку
+WrapperPoints.addEventListener('click', (event) => {
+    for (let i = 0; i < PointArea.length; i++) {
+        if (PointArea[i] === event.target || Points[i] === event.target) {
+            Points[i].classList.remove('back-white')
+            Points[i].classList.add('ring')
+            Texts[i].classList.remove('Transition-left')
+            Texts[i].classList.remove('hidden')
+            Picture[i].classList.remove('hidden')
+            Picture[i].classList.remove('Transition-left')
+        } else {
+            Points[i].classList.remove('ring')
+            Texts[i].classList.add('Transition-left')
+            Picture[i].classList.add('Transition-left')
+            //Texts[i].classList.add('hidden')
+            Picture[i].classList.add('hidden')
+        }
+    }
+})
+
+// Points[i].classList.toggle('back-white')
+//for (i = 0; i < PointArea.length; i++) {
+// if (Points[i].classList.contains('ring')) {
+//      Points[i].classList.toggle
+//  }=== event.relatedTarget && Points[i].classList.contains('ring')) {
+//})
+//}
